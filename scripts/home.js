d6e87c8aa0b4d915255df7239f22c981e292ac1f
a9d94t9h9 <axeldavidled999@gmail.com>
2022-10-05 23:32:01 -0300

    Renderizando las cards de forma dinámica con js y según fecha de evento

---
+++ scripts/home.js
@@ -0,0 +1,21 @@
+const container = document.getElementById("container");
+
+const dataEvents = data.events;
+
+for (const card of dataEvents) {
+    container.innerHTML += `
+    <div class="col p-2">
+    <div class="card" style="width: 18rem;">
+        <img src=${card.image} class="card-img-top size-img" alt="Feria de comidas">
+        <div class="card-body">
+            <h5 class="card-title">${card.name}</h5>
+            <p class="card-text hiddem-description">${card.description}</p>
+            <div class="d-flex justify-content-around">
+                <p class="card-title fw-bolder fs-4">$ ${card.price}</p>
+                <a href="#" class="btn btn-primary text-center">Read more</a>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+}
