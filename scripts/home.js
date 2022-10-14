171b29b61eadcbde22aac0b8974bc487db5ad977
a9d94t9h9 <axeldavidled999@gmail.com>
2022-10-14 00:21:13 -0300

    agregando eventos para filtrar las cards

--- scripts/home.js
+++ scripts/home.js
@@ -2,20 +2,116 @@ const container = document.getElementById("container");
 
 const dataEvents = data.events;
 
-for (const card of dataEvents) {
-    container.innerHTML += `
-    <div class="col p-2">
-    <div class="card" style="width: 18rem;">
-        <img src=${card.image} class="card-img-top size-img" alt="Feria de comidas">
-        <div class="card-body">
-            <h5 class="card-title">${card.name}</h5>
-            <p class="card-text hiddem-description">${card.description}</p>
-            <div class="d-flex justify-content-around">
-                <p class="card-title fw-bolder fs-4">$ ${card.price}</p>
-                <a href="#" class="btn btn-primary text-center">Read more</a>
+const renderAllCards = () => {
+    dataEvents.map(item => {
+        container.innerHTML += `
+        <div class="col p-2">
+        <div class="card" style="width: 18rem;">
+            <img src=${item.image} class="card-img-top size-img" alt="Feria de comidas">
+            <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text hiddem-description">${item.description}</p>
+                <div class="d-flex justify-content-around">
+                    <p class="card-title fw-bolder fs-4">$ ${item.price}</p>
+                    <a href="#" class="btn btn-primary text-center">Read more</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
-    `
+        `
+    })
 }
+
+renderAllCards()
+
+const renderCategoryFilter = category => {
+    dataEvents
+        .filter(item => item.category === category)
+        .map(item => {
+            container.innerHTML += `
+        <div class="col p-2">
+        <div class="card" style="width: 18rem;">
+            <img src=${item.image} class="card-img-top size-img" alt="Feria de comidas">
+            <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text hiddem-description">${item.description}</p>
+                <div class="d-flex justify-content-around">
+                    <p class="card-title fw-bolder fs-4">$ ${item.price}</p>
+                    <a href="#" class="btn btn-primary text-center">Read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+        })
+}
+
+/* const filterCategory = category => dataEvents.filter(item => item.category === category) */
+
+
+
+const onClickFilter = (category, CategoryString)=>{
+    //let arrConcat = []
+  if (category.checked) {
+        container.innerHTML = ''
+        renderCategoryFilter(CategoryString)
+    } else {
+        container.innerHTML = ''
+        renderAllCards()
+    }
+}
+
+
+const foodFair = document.getElementById("inlineCheckbox1")
+foodFair.addEventListener('click', function () {
+    onClickFilter(foodFair, "Food Fair")
+})
+
+const museum = document.getElementById("inlineCheckbox2")
+museum.addEventListener('click', function () {
+    onClickFilter(museum, "Museum")
+})
+
+const costumeParty = document.getElementById("inlineCheckbox3")
+costumeParty.addEventListener('click', function () {
+    onClickFilter(costumeParty, "Costume Party")
+})
+
+const musicConcert = document.getElementById("inlineCheckbox4")
+musicConcert.addEventListener('click', function () {
+    onClickFilter(musicConcert, "Music Concert")
+})
+
+const race = document.getElementById("inlineCheckbox5")
+race.addEventListener('click', function () {
+    onClickFilter(race, "Race")
+})
+
+const bookExchange = document.getElementById("inlineCheckbox6")
+bookExchange.addEventListener('click', function () {
+    onClickFilter(bookExchange, "Book Exchange")
+})
+
+const cinema = document.getElementById("inlineCheckbox7")
+cinema.addEventListener('click', function () {
+    onClickFilter(cinema, "Cinema")
+})
+
+const search = document.getElementById("inputPassword2")
+
+const renderCardsFilterSearch = (value)=>{
+  /*   dataEvents
+    .filter(item => item.category === value)
+    .map(renderAllCards) */
+    renderAllCards()
+        
+}
+
+const searchEvent = ()=>{
+    search.addEventListener('change', function(event){
+        console.log(event.target.value)
+        renderCategoryFilter(event.target.value)
+    })
+}
+
+searchEvent()
