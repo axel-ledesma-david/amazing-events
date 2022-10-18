3f7e29c4589112e269399049352bc69833829ab7
a9d94t9h9 <axeldavidled999@gmail.com>
2022-10-18 01:07:20 -0300

    Arreglo el filtrado de los checkboxes

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("container");
 const containerCheckbox = document.getElementById('containerCheckbox')
-
+const search = document.getElementById("inputPassword2")
+const submitSearch = document.getElementById('submitSearch')
 
 const dataEvents = data.events;
 
@@ -8,7 +9,7 @@ const renderAllCards = () => {
     dataEvents.map(item => {
         container.innerHTML += `
         <div class="col p-2">
-        <div class="card" style="width: 18rem;">
+        <div class="card shadow" style="width: 18rem;">
             <img src=${item.image} class="card-img-top size-img" alt="Feria de comidas">
             <div class="card-body">
                 <h5 class="card-title">${item.name}</h5>
@@ -26,32 +27,6 @@ const renderAllCards = () => {
 
 renderAllCards()
 
-/* const renderCategoryFilter = categoryEvent => {
-    dataEvents
-        .filter(item => item.category === categoryEvent)
-        .map(item => {
-            container.innerHTML += `
-        <div class="col p-2">
-        <div class="card" style="width: 18rem;">
-            <img src=${item.image} class="card-img-top size-img" alt="Feria de comidas">
-            <div class="card-body">
-                <h5 class="card-title">${item.name}</h5>
-                <p class="card-text hiddem-description">${item.description}</p>
-                <div class="d-flex justify-content-around">
-                    <p class="card-title fw-bolder fs-4">$ ${item.price}</p>
-                    <a href="#" class="btn btn-primary text-center">Read more</a>
-                </div>
-            </div>
-        </div>
-    </div>
-        `
-        })
-}
- */
-
-/* const arrCheckboxID = ['inlineCheckbox1', 'inlineCheckbox2', 'inlineCheckbox3', 'inlineCheckbox4', 'inlineCheckbox5', 'inlineCheckbox6', 'inlineCheckbox7']
- */
-
 const arrCategoryClean = new Set(dataEvents.map(item => item.category))
 
 const arrClean = Array.from(arrCategoryClean)
@@ -64,7 +39,7 @@ const renderCheckbox = () => {
 
     for (let item of arrClean) {
         containerCheckbox.innerHTML += `
-        <div class="form-check form-check-inline p-4">
+        <div class="form-check form-check-inline p-3">
         <input class="form-check-input" type="checkbox" value=${item}>
         <label class="form-check-label">${item}</label>
         </div>
@@ -76,7 +51,7 @@ renderCheckbox()
 
 let renderCategoryFilter = (value) => {
     dataEvents
-        .filter(item => item.category === value)
+        .filter(item => item.category.includes(value))
         .map(item => {
             return container.innerHTML += `
         <div class="col p-2">
@@ -96,13 +71,11 @@ let renderCategoryFilter = (value) => {
         })
 
 
-}/* 
-const checkItem = document.getElementsById('') */
+}
 
 containerCheckbox.addEventListener('click', function (e) {
 
     let valueEvent = e.target.value
-    /* let checkedValue = valueEvent.checked */
     if (valueEvent.checked = true) {
         container.innerHTML = ''
         renderCategoryFilter(valueEvent)
@@ -118,7 +91,7 @@ containerCheckbox.addEventListener('click', function (e) {
 
 
 
-const search = document.getElementById("inputPassword2")
+
 
 const renderCardsFilterSearch = (value) => {
     dataEvents
@@ -158,3 +131,21 @@ const searchEvent = () => {
 }
 
 searchEvent()
+
+
+/* 
+const submitEvent = () =>{
+    submitSearch.addEventListener('submit', function(e){
+        let valueSearch = e.target.value
+        console.log(valueSearch)
+        if (valueSearch.keyup = true) {
+            container.innerHTML = ''
+            renderCardsFilterSearch(valueSearch)
+        } else {
+            container.innerHTML = ''
+            renderAllCards()
+        }
+    })
+}
+
+submitEvent() */
